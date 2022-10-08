160748d94cb2f7bf8b494fad7089ff391811808b
sofiulaloe <sahasofiulalom3@gmail.com>
2022-10-08 08:56:45 -0700

    header.js header.css add commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo192.png';
+
 import './App.css';
 import Heeder from './component/header/Heeder';
-import Workcart from './component/workcart/Workcart';
+
 
 function App() {
   return (
