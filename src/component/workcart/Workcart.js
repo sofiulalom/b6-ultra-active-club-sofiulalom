80e122b8a0d4279fbdd3ce22639bdafa80dbde60
sofiulaloe <sahasofiulalom3@gmail.com>
2022-10-08 12:59:58 -0700

    second-cart add commit

--- src/component/workcart/Workcart.js
+++ src/component/workcart/Workcart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import './Workcart.css';
-
+import img from'./../../sofiulalom.jpg';
 
 const Workcart = () => {
     const [products,setProducts] =useState([]);
@@ -24,10 +24,42 @@ const Workcart = () => {
             
 
             ></Cart>)
-        }
-       </div>
-       </div>
-       </div>
+     }
+             </div>
+          <div className='second-cart'>
+            <div className='Cart-secondinfo'>
+            <img src={img} alt="" />
+            <h5>saha sofiul alom</h5>
+            
+
+            </div>
+            <div className='my-info'>
+            <p>i am a teacher</p>
+            <small>i am in class degre first year</small>
+            <h6>my age : 22</h6>
+             
+            </div>
+            <div>
+              <h5>Add Brack Time</h5>
+              <div className='btn-time'>
+               <button>60m</button>
+               <button>50m</button>
+               <button>30m</button>
+               <button>60m</button>
+               </div>
+            </div>
+            <div>
+              <h5>Exercise Details</h5>
+              <p className='ex-time'>Exercise time <span>  miniuts</span></p>
+                <p className='brck-time'>Breck time <span> miniuts</span></p>
+              <button className='btn-activity'>Activity Completed</button>
+            </div>
+          </div>
+        </div>
+        </div>
+
+       
+       
 
     );
 };
